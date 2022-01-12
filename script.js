9abd41bacc91513be17032f846bde78f8541c1a0
nvaisvilaite <90718352+nvaisvilaite@users.noreply.github.com>
2022-01-12 18:48:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var x = 50;
+var y = 10;
+
+var suma = x + y;
+console.log("suma: " + suma);
+
+var skirtumas = x - y;
+console.log("skirtumas: " + skirtumas);
+
+var daugyba = x * y;
+console.log("daugyba: " + daugyba);
+
+var dalyba = x / y;
+console.log("dalyba: " + dalyba);
